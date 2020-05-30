16750a76f076e495f5ea5523a7fbc6f693ad4530
Jordi València <jorval@gmail.com>
2020-05-30 20:17:26 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jordi València Asensi on 30/05/2020.
+//  Copyright © 2020 Jordi València Asensi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
